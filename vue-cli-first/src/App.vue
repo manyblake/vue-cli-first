495c56d9c0b504e55f1b5fd4339599651469fa3d
manyblake <manyblake@gmail.com>
2022-09-02 14:46:25 +0200

    first commit

---
+++ vue-cli-first/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <MainTitle />
+  </div>
+</template>
+
+<script>
+import MainTitle from "./components/MainTitle.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainTitle,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
